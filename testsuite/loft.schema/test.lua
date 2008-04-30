7fd1fb8bf336bf4ff2bd122fd51f76e48a613559
jasonsantos <jasonsantos>
2008-04-30 16:17:31 +0000

    Iniciado suporte a Schemas

---
+++ testsuite/loft.schema/test.lua
@@ -0,0 +1,41 @@
+package.path = [[;;./net.luaforge.loft/source/lua/5.1/?.lua;./net.luaforge.loft/source/lua/5.1/?/init.lua;]]
+
+
+require "loft.schema"
+
+local allTypes = Schema 'Cli' {
+
+	 Type 'tipoDocumento'
+	 	. nome "Nome"
+	 	. descricao;
+
+	 Type 'Cliente'
+		. nome
+		. endereco
+		. telefone
+		. cep : Number(256)
+		. idade
+		. dataNascimento : Date('now')
+		. tipoDocumento : Reference('tipoDocumento');
+		
+	 Type 'Assinatura'
+	 	. tipo {
+	 		Type 'TipoAssinatura'
+	 			. nome
+	 			. descricao
+	 	}
+	}
+
+for typeName, t in pairs(allTypes) do
+	print'---------------------------'
+	print(t['.typeName'])
+	print'---------------------------'
+	table.foreach(t['.fields'], function(_,f)
+		print('   -- ' .. _, type(f))
+		table.foreach(f, function(k,v)
+			print('    . ' .. k, v)		
+		end)
+
+	end)
+	print'---------------------------'
+end
