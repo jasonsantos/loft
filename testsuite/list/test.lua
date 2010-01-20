15b09218ad40eae43a258a54bafce46163b6cf20
Jason Santos <jasonsantos@gmail.com>
2010-01-20 00:13:48 -0200

    fixed the simple test for the list modules

--- testsuite/list/test.lua
+++ testsuite/list/test.lua
@@ -1,49 +1,49 @@
 package.path = ';;../../source/?.lua'
 
-require 'loft.list'
+require 'list'
 
 --TODO: create a proper test suite
 
 
-	l = create{
-		1, 2, 3, 4, 5, 6, 7,
-	}
+l = list.create{
+	1, 2, 3, 4, 5, 6, 7,
+}
 
 
-	while l:hasNext() do
-		print(l:next())
-	end
+while l:hasNext() do
+	print(l:next())
+end
 
-	l:reset() 
-	print'reset'
+l:reset() 
+print'reset'
 
-	print(l+3)
+print(l+3)
 
-	while l:hasNext() do
-		print(l())
-	end
+while l:hasNext() do
+	print(l())
+end
 
-	l:reset()
-	print'reset'
+l:reset()
+print'reset'
 
-	for o in l do
-		print(o)
-	end
+for o in l do
+	print(o)
+end
 
-	t= {}
-	for i = 33, 126 do
-	 table.insert(t, string.char(i))
-	end
+t= {}
+for i = 33, 126 do
+ table.insert(t, string.char(i))
+end
 
-	l = create{
-		count=#t,
-		mode=MODE.PAGE,
-		load=function(idx)  return t[idx] end,
-		onPageLoad=function() print'Page loaded' end
-	}
+l = list.create{
+	count=#t,
+	mode=MODE.PAGE,
+	load=function(idx)  return t[idx] end,
+	onPageLoad=function() print'Page loaded' end
+}
 
-	print(l.count, 'items')
+print(l.count, 'items')
 
-	for o in l do
-		print(o)
-	end
+for o in l do
+	print(o)
+end
