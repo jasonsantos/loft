dcb2b2c36dbb99d3763c56776c4dd3df6f3ea166
Alessandro Hecht <alessandrohc@gmail.com>
2010-02-01 20:05:35 -0200

    Added provider sqlite3

---
+++ source/loft/providers/sqlite3.lua
@@ -0,0 +1,17 @@
+module(..., package.seeall)
+
+description = [[SQLITE3 Module for Database Behaviour]]
+
+local base = require "loft.providers.base"
+
+base.sql.CREATE = [==[
+CREATE TABLE IF NOT EXISTS $table_name ( 
+  $columns{", "}[=[$escape_field_name{$column_name} $type$if{$size}[[($size)]]$if{$primary}[[ PRIMARY KEY]]$if{$required}[[ NOT NULL]]$if{$autoincrement}[[ AUTOINCREMENT]]$sep
+]=])
+]==]
+
+base.sql.INSERT = [[INSERT INTO $table_name ($data{", "}[=[$escape_field_name{$column_name}$sep]=]) VALUES ($data{", "}[=[$value$sep ]=])]]
+
+base.field_types.key={type='INTEGER', primary=true, autoincrement=true, required=false, onEscape=tonumber}
+
+return base
