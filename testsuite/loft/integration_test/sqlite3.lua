71633cd11c51a18f3e3edc83fe6372a87f076b92
Alessandro Hecht <alessandrohc@gmail.com>
2010-02-02 18:14:58 -0200

    Finishing the integration testing with sqlite3

--- testsuite/loft/integration_test/sqlite3.lua
+++ testsuite/loft/integration_test/sqlite3.lua
@@ -11,58 +11,19 @@ local loft_instance = loft.engine({
 local default = schema.expand(function ()
 	column_prefix = "f_"
 
-	info = entity {
-		table_name= "T_Info",
+	test = entity {
+		table_name= "T_Test",
 		fields = { 
 			id = { order = 1, column_name = "f_infoid", type = "key"},
-			title = { order=2, type = "text", size = 100, maxlength=250 },
-	
+			name = { order=2, type = "text", size = 100, maxlength=250 },
+			address = { order=2, type = "text", size = 100, maxlength=250 }	
 		},
 		handlers = {
 	        before_save = function(e, obj) record.lastLog = 'Saving Entity' end,
 	    },
 	}
 	
-	section = entity { 
-		fields = { 
-			id = { order = 1, colum_name = "F_SectionID", type = "key" },
-			name = { type = "text", size = 100, maxlength=250 },
-		},
-		handlers = {
-	        before_save = function(e, obj) print('!!!!!!!!') end,
-	    },
-	}	
-	
 end)
-
---loft_instance.create(default.entities.info)
-
-local info = loft_instance.new(default.entities.info)
-
-info.title = "Alessandro"
-
-loft_instance.save( info )
-
-print( info.id )
-
---print( loft_instance.find(default.entities.info).items[2].title, print )
-info.title = "Fabio"
-
-loft_instance.save( info )
-
-loft_instance.destroy( info )
-
-print( loft_instance.count(default.entities.info) )
-
-os.exit()
-
-
-provider = require"loft.providers.database.sqlite3"
-provider.options{
-	PERSISTENCE_FILENAME = 'test.db3';
-	PERSISTENCE_PATH= '/tmp/net.luaforge.loft/testsuite/'
-}
-provider.initialize('test.db3')
 
 local now = os.clock()
 local log = function()
@@ -74,22 +35,31 @@ local lfs = require'lfs'
 local NUM_OF_OBJECTS_TO_GENERATE = 100
 
 -- generates an ID
-local id = provider.getNextId('test')
-local firstId = id
 local times = {}
 local duplicatesTimes = {}
+local firstId
+local id
+loft_instance.create(default.entities.test)
 
+table.foreachi(loft_instance.find(default.entities.test).items, function(i, item)
+	assert(loft_instance.destroy( item, true ))
+end)
+ 
 for i=1, NUM_OF_OBJECTS_TO_GENERATE do
 	-- persists a given table
-	provider.persist('test', id, {
-		id=id; 
-		name='test'..i; 
-		address= (i*347) .. ', Elm Street';
-	})
+	local l = loft_instance.new(default.entities.test)
+	l.name = 'test'..i; 
+	l.address = (i*347) .. ', Elm Street';
+	loft_instance.save(l)
+	id = l.id
+	
+	if (not firstId ) then
+		firstId = id
+	end
+	
 	-- counts how many times this particular ID has been generated
 	table.insert(times, id)
 	duplicatesTimes[id] = (duplicatesTimes[id] or 0) + 1
-	id = provider.getNextId('test')
 end
  
 local generatedId = firstId
@@ -99,85 +69,90 @@ table.foreachi(times, function(k, v)
 	generatedId = generatedId + 1
 end)
 
-local lastId = firstId + NUM_OF_OBJECTS_TO_GENERATE
+local lastId = firstId + NUM_OF_OBJECTS_TO_GENERATE - 1
 
 assert(id==lastId, 'wrong number of objects created')
 
-local t = provider.retrieve('test', lastId-1)
-assert(t, 'object was not retrieved for id==' .. lastId)
-assert(t.name == 'test' .. (lastId-firstId), 'The proper object was not retrieved')
+--~ local t = loft_instance.get(default.entities.test, lastId)
+
+--~ local t2 = loft_instance.find(default.entities.test, {filters = {id = lastId}})
+
+--~ assert( t.id == t2.id, 'check equals data in get and find')
+--~ assert( t.name == t2.name, 'check equals data in get and find' )
+
+--~ assert(t.id, 'object was not retrieved for id==' .. lastId)
+--~ assert(t.name == 'test' .. (lastId-firstId), 'The proper object was not retrieved')
 
 local list = {}
 
-provider.search('test', {name='test99'}, function(item)
-	assert(item.name=='test99')
+table.foreachi(loft_instance.find(default.entities.test, {filters = {name='test98'}}).items, function(i, item)
+	assert(item.name=='test98')
 	table.insert(list, item)
 end)
 
 assert(#list==1, 'search did not find the item by name')
 
 table.foreachi(list, function(i, item)
-	provider.erase('test', item.id)
+	loft_instance.destroy( item )
 end)
 
-list = provider.search('test', {name='test99'}, function(item)
-	assert(item.name=='test99')
+list = loft_instance.find(default.entities.test, {filters = {name='test98'}, visitor = function(item)
+	assert(item.name=='test98')
 	table.insert(list, item)
-end)
+end})
 
 assert(#list==0, 'erase did not remove the item properly')
 
-list = provider.search('test', {name='test98'})
+list = loft_instance.find(default.entities.test, {filters = {name='test98'}}).items
 
 assert(#list==1, 'short search did not find the item by name')
 
-table.foreachi(list, function(i, item)
-	provider.erase('test', item.id)
-end)
+loft_instance.destroy( list[1], true )
 
-list = provider.search('test', {name='test98'})
+list = loft_instance.find(default.entities.test, {filters = {name='test98'}}).items
 
 assert(#list==0, 'erase did not remove the item properly')
 
-
 local NumLimit = 2
 for i=1, NUM_OF_OBJECTS_TO_GENERATE / NumLimit do
-	list = provider.search('test', { 
-		__sort='+id',
-		__offset = i - 1,
-		__limit = NumLimit,
-	 })
+	list = loft_instance.find(default.entities.test, { 
+		pagination = {
+			offset = i - 1,
+			limit = NumLimit
+		},
+		sorting = {"+id"}
+	 }).items
 	 table.foreachi(list, function (v, t)
 		assert(t.name == "test" .. i)
 		i = i + 1
 	 end)
 end
 
-_count = provider.count('test', {name='test98'})
+_count = loft_instance.count(default.entities.test, {filters = {name='test98'}})
 
 assert(_count==0, 'erase did not remove the item properly')
 
-assert( provider.count('test', {name='test1'}) == 1)
+assert( loft_instance.count(default.entities.test, {filters = {name='test1'}}) == 1)
 
-list = provider.search('test', {__sort='-name', __limit=10})
+list = loft_instance.find(default.entities.test, { sorting = {'-name'}, pagination = {limit=10}})
 
 assert(#list==10, 'limited search has brought the wrong amount of items')
 
-assert( provider.count('test', {COLUMN_WRONG='test1'}) == 0)
+assert( pcall( loft_instance.count, default.entities.test, { filters = {COLUMN_WRONG='test1'}}) == false)
 
-_count = provider.count('test')
+_count = loft_instance.count(default.entities.test)
 
 assert(_count, 'global search found the wrong amount of items')
 
-list = provider.search('test', {__sort='-name'})
+list = loft_instance.find(default.entities.test, { sorting = {'-name'}}).items
 
-assert(#list==98, 'global search found the wrong amount of items')
+assert(#list==99, 'global search found the wrong amount of items')
 
 table.foreachi(list, function(i, item)
-	provider.erase('test', item.id)
+	assert(loft_instance.destroy( item, true ) ~= false, "Don't search and destroy objects loft")
 end)
 
-list = provider.search('test', {})
+list = loft_instance.find(default.entities.test).items
 
 assert(#list==0, 'erase did not remove all items properly')
 
